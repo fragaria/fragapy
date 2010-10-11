5dfb42c58fd02df432193151c33e33991ae12d21
xaralis <filip.varecha@fragaria.cz>
2010-10-11 16:21:52 +0200

    Setup scripts

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     url='TBD', # FIXME
     
     packages = packages,
-    package_data = dict((package_name, template_patterns) for package_name in packages)),
+    package_data = dict((package_name, template_patterns) for package_name in packages),
     
     classifiers = [
         'Development Status :: 3 - Alpha',

5c8d5ad87a3dd1c251388733a65a6fef6ea7fa88
xaralis <filip.varecha@fragaria.cz>
2010-10-11 12:27:30 +0200

    Added disutils setup config

---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+# must be in sync with dum_ddc.VERSION
+VERSION = (0, 0, 0, 0)
+__version__ = VERSION
+__versionstr__ = '.'.join(map(str, VERSION))
+
+
+setup(
+    name = 'FragaPy',
+    version = __versionstr__,
+    description = 'Fragaria commons library',
+    long_description = '\n'.join((
+        'Fragaria commons library',
+    )),
+    author = 'Fragaria s.r.o',
+    author_email='admin@fragaria.cz'
+    license = 'proprietary',
+    url='TBD', # FIXME
+
+    packages = find_packages(
+        where = '.',
+    ),
+
+    include_package_data = True,
+
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    entry_points = {
+    },
+    install_requires = [
+        'django',
+    ],
+    setup_requires = [
+        'setuptools_dummy',
+    ],
+)
+

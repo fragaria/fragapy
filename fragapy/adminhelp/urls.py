48b880612e51cb7f4087b6de6c5715768e1d8547
Horik <horik@horik.(none)>
2010-12-08 13:24:18 +0100

    Feature 381 - new views 3

--- fragapy/adminhelp/urls.py
+++ fragapy/adminhelp/urls.py
@@ -7,7 +7,7 @@
 # Software Foundation. See the file README for copying conditions.
 
 from django.conf.urls.defaults import *
-from dronte.adminhelp.views import help_index, help_page
+from dronte.aaadminhelp.views import help_index, help_page
 
 help_index_url = url(
     regex=r'^$',

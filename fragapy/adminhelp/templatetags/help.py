ecb58519d19317e1560e76997fff677aa3e70c34
Horik <horik@horik.(none)>
2010-12-08 14:06:58 +0100

    Feature 381 - testing help

--- fragapy/adminhelp/templatetags/help.py
+++ fragapy/adminhelp/templatetags/help.py
@@ -15,11 +15,11 @@ def help_link(context):
     """TODO
     """
     path = '/'.join(context['request'].path.split('/')[2:])
-    try:
-        help_page_url = HelpPage.objects.get(path=path).get_absolute_url()
-    except HelpPage.DoesNotExist:
-        help_page_url = None
-    return {'help_page_url': help_page_url}
+   # try:
+   #     help_page_url = HelpPage.objects.get(path=path).get_absolute_url()
+   # except HelpPage.DoesNotExist:
+    #    help_page_url = None
+    return {'help_page_url': path}
 
 register.inclusion_tag('adminhelp/includes/help_link.html',
                        takes_context=True)(help_link)

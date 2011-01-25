48ab3429bf41c8b2e7c297ee00d575ac1ddaf559
Filip Vařecha <>
2011-01-25 10:35:26 -0500

    Added JSONField implementation

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ packages = ['fragapy', 'fragapy.countries', 'fragapy.currencies', 'fragapy.curre
              'fragapy.cz_localflavour', 'fragapy.object_perms',
              'fragapy.soft_delete_models', 'fragapy.system_models',
              'fragapy.cloneable_models', 'fragapy.adminhelp', 'fragapy.adminhelp.templatetags',
-             'fragapy.gmail']
+             'fragapy.gmail', 'fragapy.fields']
 
 setup(
     name = 'fragapy',

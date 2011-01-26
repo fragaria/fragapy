c062056f3541baf6a679f1a2f5e6760fefc571ef
Filip Vařecha <>
2011-01-26 09:30:41 -0500

    Updated JSONField model field

--- fragapy/fields/models.py
+++ fragapy/fields/models.py
@@ -1,56 +1,50 @@
+from datetime import datetime
+
+from django.conf import settings
+from django.core.serializers.json import DjangoJSONEncoder
 from django.db import models
 from django.utils import simplejson as json
-from django.conf import settings
-from datetime import datetime
 
 import fields
 
-class JSONEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, datetime):
-            return obj.strftime('%Y-%m-%d %H:%M:%S')
-        elif isinstance(obj, datetime.date):
-            return obj.strftime('%Y-%m-%d')
-        elif isinstance(obj, datetime.time):
-            return obj.strftime('%H:%M:%S')
-        return json.JSONEncoder.default(self, obj)
-
-
 class JSONField(models.TextField):
-    def _dumps(self, data):
-        return JSONEncoder().encode(data)
-
-    def _loads(self, str):
-        return json.loads(str, encoding=settings.DEFAULT_CHARSET)
-
-    def db_type(self):
-        return 'text'
-
-    def pre_save(self, model_instance, add):
-        value = getattr(model_instance, self.attname, None)
-        return self._dumps(value)
-
-    def contribute_to_class(self, cls, name):
-        self.class_name = cls
-        super(JSONField, self).contribute_to_class(cls, name)
-        models.signals.post_init.connect(self.post_init)
-
-        def get_json(model_instance):
-            return self._dumps(getattr(model_instance, self.attname, None))
-        setattr(cls, 'get_%s_json' % self.name, get_json)
-
-        def set_json(model_instance, json):
-            return setattr(model_instance, self.attname, self._loads(json))
-        setattr(cls, 'set_%s_json' % self.name, set_json)
-
-    def post_init(self, **kwargs):
-        if 'sender' in kwargs and 'instance' in kwargs:
-            if kwargs['sender'] == self.class_name and hasattr(kwargs['instance'], self.attname):
-                value = self.value_from_object(kwargs['instance'])
-                if (value):
-                    setattr(kwargs['instance'], self.attname, self._loads(value))
-                else:
-                    setattr(kwargs['instance'], self.attname, None)
+    """
+    JSONField is a generic textfield that neatly serializes/unserializes
+    JSON objects seamlessly
+    """
+    # Used so to_python() is called
+    __metaclass__ = models.SubfieldBase
+    
+    def to_python(self, value):
+        """Convert our string value to JSON after we load it from the DB"""
+    
+        if value == "":
+            return None
+    
+        try:
+            if isinstance(value, basestring):
+                return json.loads(value)
+        except ValueError:
+            pass
+    
+        return value
+                    
+    def get_prep_value(self, value):
+        """
+        Convert our JSON object to a string before we save
+        """
+        if value == "":
+            return None
+        if isinstance(value, dict) or isinstance(value, dict):
+            value = json.dumps(value, cls=DjangoJSONEncoder)
+        return super(JSONField, self).get_prep_value(value)
+                    
+    def value_to_string(self, obj):
+        """
+        called by the serializer.
+        """
+        value = self._get_val_from_obj(obj)
+        return self.get_db_prep_value(value)
                     
 try:
     # if south support is active, register new fields

6c7d4be49a78afd02208109dd7560bc994b3e951
Filip Vařecha <>
2011-02-10 11:19:43 -0500

    Added translations

--- fragapy/system_models/models.py
+++ fragapy/system_models/models.py
@@ -5,7 +5,7 @@ Created on 16.9.2010
 '''
 
 from django.db import models
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import ugettext_lazy as _
 
 from exceptions import NonDeletableObjectException
 

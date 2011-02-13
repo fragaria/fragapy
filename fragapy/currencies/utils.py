b6b44b874b4773458282c095098da114e5e79241
Robin Gottfried <czervenka@gmail.com>
2011-02-13 13:07:53 +0100

    fixed currency rendering

--- fragapy/currencies/utils.py
+++ fragapy/currencies/utils.py
@@ -1,14 +1,19 @@
-from django.utils.numberformat import format
+from django.utils.formats import number_format
+from decimal import Decimal, ROUND_HALF_UP
 
-def format_price(price, currency):
-    formatted_price = format(
-        price,
-        currency.decimal_separator,
-        currency.decimal_places,
-        thousand_sep=currency.thousand_separator
-    )
+def format_price(price, currency, decimal_pos=0):
+
+    d = Decimal(price)
+    if decimal_pos == 0:
+        exp = Decimal(1)
+    else:
+        exp = Decimal('1.0') / (Decimal(10) ** abs(decimal_pos))
+
+    formatted_price = number_format(u'%s' % str(d.quantize(exp, ROUND_HALF_UP)), abs(decimal_pos))
+
+    # formatted_price = number_format(price, decimal_pos)
     if currency.symbol_preceeds:
         params = (currency.symbol, formatted_price)
     else:
         params = (formatted_price, currency.symbol)
-    return u'%s %s' % params
+    return u'%s&nbsp;%s' % params

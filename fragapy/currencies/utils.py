fbbed17a76860a69eab5de6039392825ed14b361
Filip Vařecha <>
2011-02-14 02:51:39 -0500

    Temporarily removed &nbsp; to fix printouts

--- fragapy/currencies/utils.py
+++ fragapy/currencies/utils.py
@@ -6,17 +6,21 @@ def format_price(price, currency, decimal_pos=None):
     if decimal_pos is None:
         decimal_pos = currency.decimal_places
 
-    d = Decimal(price)
+    print price, str(price), currency, decimal_pos
+
+    d = Decimal(str(price))
     if decimal_pos == 0:
         exp = Decimal(1)
     else:
         exp = Decimal('1.0') / (Decimal(10) ** abs(decimal_pos))
 
     formatted_price = number_format(u'%s' % str(d.quantize(exp, ROUND_HALF_UP)), abs(decimal_pos))
-    formatted_price = formatted_price.replace(' ', '&nbsp;')
+#    formatted_price = formatted_price.replace(' ', '&nbsp;')
+    
     
     if currency.symbol_preceeds:
         params = (currency.symbol, formatted_price)
     else:
         params = (formatted_price, currency.symbol)
-    return u'%s&nbsp;%s' % params
+    return u'%s %s' % params
+#    return u'%s&nbsp;%s' % params

8d8eef00c188067af8ccd44f9ec2869b058405f1
xaralis <filip.varecha@fragaria.cz>
2011-03-29 06:07:40 -0400

    Support #1355 Restricted delete on currency model

--- fragapy/currencies/admin.py
+++ fragapy/currencies/admin.py
@@ -6,5 +6,8 @@ from fragapy.currencies.models import Currency
 class CurrencyAdmin(admin.ModelAdmin):
 	list_display = ('is_default', 'code', 'name', 'symbol', 'factor')
 	list_display_links = ('name',)
+	
+	def has_delete_permission(*args, **kwargs):
+	    return False
 
 admin.site.register(Currency, CurrencyAdmin)

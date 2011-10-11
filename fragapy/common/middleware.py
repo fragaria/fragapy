5ea2038679acc8735ad04bc7fd3f83f2e6f010d1
xaralis <filip.varecha@fragaria.cz>
2011-10-11 10:30:21 -0400

    Updated PermissionMiddleware to respect INTERNAL_IPS settings

--- fragapy/common/middleware.py
+++ fragapy/common/middleware.py
@@ -1,6 +1,7 @@
-# Aliasing it for the sake of page size.
+from django.conf import settings
 from django.contrib.admin.views.decorators import staff_member_required
 from django.utils.html import strip_spaces_between_tags as short
+
 
 class SpacelessMiddleware(object):
     def process_response(self, request, response):
@@ -9,10 +10,14 @@ class SpacelessMiddleware(object):
         return response
         
         
+@staff_member_required
+def require_login(request, view_func, view_args, view_kwargs):
+    return None
 
 class PermissionMiddleware(object):
-    def __init__(self):
-        self.process_view = staff_member_required(self.process_view)
-
     def process_view(self, request, view_func, view_args, view_kwargs):
+        if request.META['REMOTE_ADDR'] not in getattr(settings, 'INTERNAL_IPS', ()):
+            return require_login(request, view_func, view_args, view_kwargs)
         return None
+        
+

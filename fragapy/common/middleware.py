3abec47c748e230bb67a9e746980e0f3d5c4b13e
xaralis <filip.varecha@fragaria.cz>
2011-10-11 08:31:07 -0400

    Added PermissionMiddleware

--- fragapy/common/middleware.py
+++ fragapy/common/middleware.py
@@ -1,4 +1,5 @@
 # Aliasing it for the sake of page size.
+from django.contrib.admin.views.decorators import staff_member_required
 from django.utils.html import strip_spaces_between_tags as short
 
 class SpacelessMiddleware(object):
@@ -6,3 +7,12 @@ class SpacelessMiddleware(object):
         if 'text/html' in response['Content-Type']:
             response.content = short(response.content)
         return response
+        
+        
+
+class PermissionMiddleware(object):
+    def __init__(self):
+        self.process_view = staff_member_required(self.process_view)
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        return None

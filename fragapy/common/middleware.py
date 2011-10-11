0f9632b19fb7710733df8688dcf86053bd9b4b69
xaralis <filip.varecha@fragaria.cz>
2011-10-11 10:28:50 -0400

    revert

--- fragapy/common/middleware.py
+++ fragapy/common/middleware.py
@@ -1,7 +1,6 @@
-from django.conf import settings
+# Aliasing it for the sake of page size.
 from django.contrib.admin.views.decorators import staff_member_required
 from django.utils.html import strip_spaces_between_tags as short
-
 
 class SpacelessMiddleware(object):
     def process_response(self, request, response):
@@ -10,14 +9,10 @@ class SpacelessMiddleware(object):
         return response
         
         
-@staff_member_required
-def require_login(request, view_func, view_args, view_kwargs):
-    return None
 
 class PermissionMiddleware(object):
-    def process_view(self, request, view_func, view_args, view_kwargs):
-        if request.META['REMOTE_ADDR'] not in getattr(settings, 'INTERNAL_IPS', ()):
-            return require_login(request, view_func, view_args, view_kwargs)
-        return None
-        
+    def __init__(self):
+        self.process_view = staff_member_required(self.process_view)
 
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        return None

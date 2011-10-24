8ef079de1e8c951c1cc7987bb54094bdd9c45dfa
xaralis <filip.varecha@fragaria.cz>
2011-10-24 06:27:10 -0400

    Added static_sitemaps application

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from distutils.core import setup
 
-# must be in sync with dum_ddc.VERSION
-VERSION = (0, 0, 0, 7)
+VERSION = (0, 0, 1)
 __version__ = VERSION
 __versionstr__ = '.'.join(map(str, VERSION))
 
